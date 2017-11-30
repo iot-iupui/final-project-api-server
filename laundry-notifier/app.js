c802236dd4166e95b8d21b203134dc243ef56212
nrhubbar <nrhubbar@iupui.edu>
2017-11-29 19:28:58 -0800

    Updated app to use cors

--- laundry-notifier/app.js
+++ laundry-notifier/app.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 var index = require('./routes/index');
 
 var app = express();
 
-
+app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
